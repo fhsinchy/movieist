3a2c8898e23821bf3e51d8a3146cd0f32afde1b1
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-01-04 19:32:16 +0600

    Init

---
+++ src/main/java/dev/farhan/movieist/MovieistApplication.java
@@ -0,0 +1,13 @@
+package dev.farhan.movieist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/farhan/movieist/MovieistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farhan.movieist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
