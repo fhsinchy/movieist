84c3a203b22faf941341eece9eb094a6626939b8
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-01-11 00:54:49 +0600

    Added dotenv and database configuration

--- src/main/java/dev/farhan/movieist/MovieistApplication.java
+++ src/main/java/dev/farhan/movieist/MovieistApplication.java
@@ -2,12 +2,20 @@ package dev.farhan.movieist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieistApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
 	}
 
 }
