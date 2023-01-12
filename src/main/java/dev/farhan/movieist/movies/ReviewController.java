230a3ac069c6e0c450dec3f2ee027c51e1ad9a66
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2023-01-12 21:33:45 +0600

    Added CORS configuration

--- src/main/java/dev/farhan/movieist/movies/ReviewController.java
+++ src/main/java/dev/farhan/movieist/movies/ReviewController.java
@@ -3,10 +3,7 @@ package dev.farhan.movieist.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
